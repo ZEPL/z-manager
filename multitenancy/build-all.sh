4964ced0a08638c0d52ef0039903f75c486137d7
Alexander Bezzubov <abezzubov@nflabs.com>
2015-11-25 13:51:21 +0900

    Update build script

--- multitenancy/build-all.sh
+++ multitenancy/build-all.sh
@@ -41,24 +41,28 @@ echo "cd ../server"
 cd server
 
 export GOPATH_BIN='../third_party/go/bin'
-if [[ ! -f "$GOPATH_BIN/go-bindata" ]] ; then
+if [[ ! -f "${GOPATH_BIN}/go-bindata" ]] ; then
   go get -u github.com/jteeuwen/go-bindata/...
+  echo "Downloading 'go-bindata'"
   if [[ "$?" -ne 0 ]]; then
     echo "Unable to 'go get' github.com/jteeuwen/go-bindata" >&2
     exit "${E_BAD_GOGET}"
   fi
+  echo "Done."
 fi
 
 echo "generating binary data assets"
-"$GOPATH_BIN/go-bindata" -o ./assets.go ../web/ui/public/**
+"${GOPATH_BIN}/go-bindata" -o ./assets.go ../web/ui/public/**
 if [[ "$?" -ne 0 ]]; then
   echo "Unable to generate assets.go using go-bindata" >&2
   exit "${E_BAD_ASSETGEN}"
 fi
 
 echo "go build ./..."
-go build ./...
-if [[ "$?" -ne 0 ]]; then
+if ! go build ./... ; then
   echo "Unable to 'go build' the server" >&2
   exit "${E_BAD_GOBUILD}"
+else
+  echo "Done."
+  echo "Binary is in ./server/server, you can run in dev mode ./server/z-manager.sh"
 fi
